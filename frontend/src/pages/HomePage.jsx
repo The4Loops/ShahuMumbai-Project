0a38b168b3f4a60404f09898e89702495a080077
DilipSingh004 <singhdilip9833@gmail.com>
2025-08-18 19:16:51 +0530

    .

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,13 +3,11 @@ import Layout from "../layout/Layout";
 import Hero from "../components/Hero";
 import Featured from "../components/Featured"; 
 import Collections from "../components/Collections";
-import Newsletter from "../components/Newsletter";
  
 function HomePage() {
   return (
     <Layout>
       <Hero />
-      <Newsletter collapsible defaultOpen={false} />
       <Featured />
       <Collections />
     </Layout>
